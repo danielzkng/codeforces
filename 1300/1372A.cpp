07ff12deac31e2b40a2bf6bb2e85d79f919242fe
danielzkng <danielzkng0@gmail.com>
2021-10-17 16:59:41 -0400

    Round 655 (div. 2)

---
+++ 1300/1372A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int iter = 0; iter < n; ++iter){
+        int k;
+        cin >> k;
+        for(int i = 0; i < k; ++i){
+            cout << "251 ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
