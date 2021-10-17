74d65d2b95a676afec9dc6023b6daaf96e9c05d5
danielzkng <danielzkng0@gmail.com>
2021-10-17 15:45:42 -0400

    Educational Round 63

---
+++ 1100/1155A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    string s;
+    cin >> s;
+    for(int i = 1; i < n; ++i){
+        if(s[i] < s[i - 1]) {cout << "YES\n" << i << " " << i + 1 << "\n";
+    
+        return 0;}
+    }
+    cout << "NO\n";
+    return 0;
+}
