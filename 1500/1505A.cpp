a83ad205a86a53759d80450ce0c851901bf6ac5b
danielzkng <danielzkng0@gmail.com>
2021-10-16 23:52:17 -0400

    April Fool's 2021

---
+++ 1500/1505A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    while(!cin.eof()){
+        string s;
+        cin >> s;
+        
+        cout << "NO" << endl;
+        cout << flush;
+    }
+    return 0;
+}
